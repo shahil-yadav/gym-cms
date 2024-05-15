1884c9bd7af3894a8df5f712e102d790c8d5e400
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-05-15 19:43:40 +0530

    Fixed Mobile Navigation

---
+++ src/scripts/header+top-btn.js
@@ -0,0 +1,16 @@
+/**
+ * header & back top btn active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
--- src/scripts/navbar-toggle.js
+++ src/scripts/navbar-toggle.js
@@ -31,20 +31,3 @@ const closeNavbar = function () {
 };
 
 addEventOnElem(navLinks, "click", closeNavbar);
-
-/**
- * header & back top btn active
- */
-
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-});
